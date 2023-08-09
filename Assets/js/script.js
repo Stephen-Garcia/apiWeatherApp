787421c0bedf509aff36dd08a388264df037028c
Stephen Garcia <garciastephen5280@gmail.com>
2023-08-08 23:50:17 -0600

    started javascript

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,8 @@
-//First commit
+var resultTextEl = document.querySelector('#result-text');
+var resultContentEl = document.querySelector('#result-content');
+var searchFormEl = document.querySelector('#search-form');
+
+function getParams() {
+    let searchParamsArr = document.location.search.split('&');
+    
+}
