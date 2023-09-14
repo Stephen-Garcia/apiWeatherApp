d2833a472c566a1e9c75f099433f0095b6e19bcf
Stephen Garcia <garciastephen5280@gmail.com>
2023-09-13 23:59:06 -0600

    added som css names

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -30,3 +30,91 @@ function searchApi(lat, lon) {
 };
 
 submitEl.addEventListener('click', getApi);
+
+
+
+
+// document.getElementById('recipe-search-form').addEventListener('submit', function (event) {
+//     event.preventDefault();
+
+//     const query = searchNutritionEl.value;
+//     const dietaryPreference = document.getElementById('dietary-preference').value;
+
+//     getRecipes(query, dietaryPreference);
+// });
+
+
+// submitEl.addEventListener('click', getRecipes);
+
+
+// var apiKey = "a4bf9990bcf9632efd2074e14211ea95";
+
+
+// var searchBarEl = document.querySelector('#search-bar');
+// var searchFormEl = document.querySelector('#search-form');
+// var searchHistoryEl = document.querySelector('#search-history');
+// var currentWeatherEl = document.querySelector('#current-weather');
+// var forecastContainerEl = document.querySelector('#forecast-container');
+
+
+// function getApi(city) {
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             var lat = data.coord.lat;
+//             var lon = data.coord.lon;
+//             searchApi(lat, lon, city);
+//         });
+// }
+
+
+// function searchApi(lat, lon, city) {
+//     fetch('https://api.openweathermap.org/data/2.5/forecast?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             displayWeather(data, city);
+//         });
+// }
+
+
+// function displayWeather(data, city) {
+//     var currentWeather = data.list[0];
+//     currentWeatherEl.innerHTML = `
+//         <h2>${city} (${new Date().toLocaleDateString()}) <img src="https://openweathermap.org/img/w/${currentWeather.weather[0].icon}.png" alt="${currentWeather.weather[0].description}"></h2>
+//         <p>Temperature: ${(currentWeather.main.temp - 273.15).toFixed(2)} °C</p>
+//         <p>Humidity: ${currentWeather.main.humidity}%</p>
+//         <p>Wind Speed: ${currentWeather.wind.speed} m/s</p>
+//     `;
+
+   
+//     forecastContainerEl.innerHTML = '';
+//     for (var i = 0; i < data.list.length; i += 8) {
+//         var forecast = data.list[i];
+//         var forecastDate = new Date(forecast.dt * 1000);
+//         forecastContainerEl.innerHTML += `
+//             <div class="forecast-card">
+//                 <p>${forecastDate.toLocaleDateString('en-US', { weekday: 'short', month: 'short', day: 'numeric' })}</p>
+//                 <img src="https://openweathermap.org/img/w/${forecast.weather[0].icon}.png" alt="${forecast.weather[0].description}">
+//                 <p>Temperature: ${(forecast.main.temp - 273.15).toFixed(2)} °C</p>
+//                 <p>Humidity: ${forecast.main.humidity}%</p>
+//                 <p>Wind Speed: ${forecast.wind.speed} m/s</p>
+//             </div>
+//         `;
+//     }
+// }
+
+
+// function handleSearchFormSubmit(event) {
+//     event.preventDefault();
+//     var city = searchBarEl.value.trim();
+//     if (city) {
+//         getApi(city);
+//         searchBarEl.value = '';
+//     }
+// }
+
+// searchFormEl.addEventListener('submit', handleSearchFormSubmit);
